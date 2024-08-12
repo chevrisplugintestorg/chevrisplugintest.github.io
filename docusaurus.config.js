7b2a6c48ae78afad49925cb2b2cc5b486229239d
chevris <tevachevris@gmail.com>
2024-08-12 11:23:36 +0200

    wip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'chevrisplugintest.github.io Site',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Site tagline',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
